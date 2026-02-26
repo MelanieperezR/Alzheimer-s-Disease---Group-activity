544bc62df29674cb7e396f53633f7dfc1d1f09f1
MelanieperezR <melanieperezrojano@gmail.com>
2026-02-26 14:27:11 -0500

    Update Code Dashboard Alzehimer.py

--- Code Dashboard Alzehimer.py
+++ Code Dashboard Alzehimer.py
@@ -73,10 +73,10 @@ if df is not None:
     # Barra lateral - Información de Autoría y Filtros
     st.sidebar.markdown("### Integrantes del Proyecto")
     st.sidebar.markdown("""
-    * Valentina Torres
-    * Melanie Paola Perez 
-    * Natalia Sojo 
-    * Dana Valentina Ramirez
+    * Valentina Torres Lugo
+    * Melanie Perez Rojano
+    * Natalia Sojo Jimenez
+    * Dana Ramirez Suarez
     """)
     st.sidebar.divider()
  
